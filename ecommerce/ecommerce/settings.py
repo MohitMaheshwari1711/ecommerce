e60712df9688951e47a65801263f0a0213cd706b
Mohitmaheshwari1711 <mohitmaheshwari1711@gmail.com>
2020-07-23 19:10:28 +0530

    Minor fix

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '039)rustf66ybi5!%1&+cpnpvi&cew=881)tt!xs0$#kq451rq'
+SECRET_KEY = '***************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
